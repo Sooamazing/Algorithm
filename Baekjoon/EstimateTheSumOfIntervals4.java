f63658399dc4069071b3ac8a14fecfe2e54a251b
goorm <honang515@gmail.com>
2023-12-01 21:19:57 +0900

    백준 - 구간합 구하기 4 코드만.

---
+++ Baekjoon/EstimateTheSumOfIntervals4.java
@@ -0,0 +1,35 @@
+// https://www.acmicpc.net/problem/11659
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class EstimateTheSumOfIntervals4 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] sum = new int[N + 1];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i < N + 1; i++) {
+			sum[i] += (sum[i - 1] + Integer.parseInt(st.nextToken()));
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+
+			sb.append(sum[end] - sum[start - 1]).append('\n');
+		}
+
+		System.out.println(sb);
+	}
+}
