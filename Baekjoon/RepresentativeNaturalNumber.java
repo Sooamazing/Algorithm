8260c4c55060df279e1e676b53fef8e1d8739ecf
Sooamazing <honang515@gmail.com>
2024-02-26 16:22:54 +0900

    백준 - 대표 자연수, 코드만. - min, max 기준 설정에 유의. - SET에 Array 집어넣는 법 -> Arrays.asList(count) X, Arrays.stream(count).boxed().collect(Collectors.toCollection(TreeSet::new))   - wrapper라서 감싸야 함. 더 알아보기. 그리고, 이게 더 느림. 왜일까? 그럼 이건 언제 적절하지?

--- Baekjoon/RepresentativeNaturalNumber.java
+++ Baekjoon/RepresentativeNaturalNumber.java
@@ -3,23 +3,25 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 // https://www.acmicpc.net/problem/2548
 public class RepresentativeNaturalNumber {
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int[] count = new int[N];
-		Set<Integer> set = new TreeSet<>();
-
 		String[] split = br.readLine().split(" ");
+
 		for (int i = 0; i < N; i++) {
 			count[i] = Integer.parseInt(split[i]);
-			set.add(count[i]);
 		}
 
+		Set<Integer> set = Arrays.stream(count).boxed().collect(Collectors.toCollection(TreeSet::new));
 		int min = Integer.MAX_VALUE;
 		int result = 0;
 		for (Integer std : set) {
