a5798c0c286751992ff0a83ff44f1080da11c508
Sooamazing <honang515@gmail.com>
2024-03-05 03:26:42 +0900

    백준 - 퇴사, 코드만. - N+1로 변경. 여전히, 이게 맞나 싶은데... 뭔가... 비효율적인데 말야 하하...

--- Baekjoon/Resignation.java
+++ Baekjoon/Resignation.java
@@ -4,27 +4,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// https://www.acmicpc.net/problem/14501
 public class Resignation {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] day = new int[N + 1];
-		int[] cost = new int[N + 1];
+		int[] day = new int[N + 2];
+		int[] cost = new int[N + 2];
 		int[] result = new int[N + 2];
 
-		int max = 0;
 		for (int i = 1; i < N + 1; i++) {
-
 			// 입력
 			String[] split = br.readLine().split(" ");
 			day[i] = Integer.parseInt(split[0]);
 			cost[i] = Integer.parseInt(split[1]);
+		}
 
-			// N+1 이후의 날짜는 무조건 퇴사일을 넘어가므로 무시
-			if (N + 1 < i + day[i]) {
-				continue;
-			}
-
+		for (int i = 1; i <= N + 1; i++) {
 			// i일 전까지의 최대 이익과 현재의 최대 이익을 비교해 최대 이익을 구함
 			for (int j = 1; j < i; j++) {
 				if (result[j] > result[i]) {
@@ -32,16 +28,20 @@ public class Resignation {
 				}
 			}
 
+			// N 이후의 날짜는 무조건 퇴사일을 넘어가므로 무시
+			if (N + 1 < i + day[i]) {
+				continue;
+			}
+
 			// i일에 일을 했을 때의 이익을 구함
 			int exp = result[i] + cost[i];
 			if (result[i + day[i]] < exp) {
 				result[i + day[i]] = exp;
-				max = Math.max(max, exp);
 			}
 
 		}
 
-		System.out.println(max);
+		System.out.println(result[N + 1]);
 
 	}
 }
