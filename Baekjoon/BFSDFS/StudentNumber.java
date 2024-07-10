b08e7762d04ac878e23bbdab94e98546310a3214
Sooamazing <honang515@gmail.com>
2024-07-11 00:37:39 +0900

    백준 - (참고)학생 번호, 코드만. - 블로그 참고 -> set 자료 구조 사용, 중복 개수 검사. - 3중 for문으로 전체 비교 - 00 때문에 int형 불가, String으로 비교.

--- Baekjoon/BFSDFS/StudentNumber.java
+++ Baekjoon/BFSDFS/StudentNumber.java
@@ -3,8 +3,14 @@ package Baekjoon.BFSDFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
-// 24-07-10 23:40
+// 3
+// 123
+// 213
+// 323 -> 문제 이해에서 계속 실수. 같/다
+// 24-07-10 35min
 // https://www.acmicpc.net/problem/1235
 public class StudentNumber {
 	public static void main(String[] args) throws IOException {
@@ -13,36 +19,59 @@ public class StudentNumber {
 		// 학생 수
 		int studentNum = Integer.parseInt(br.readLine());
 
-		String student = br.readLine();
-		int length = student.length();
-		// int[][] students = new int[studentNum][length];
-		String[][] students = new String[studentNum][length];
-		students[0][0] = student.charAt(0) + "";
-		for (int j = 1; j < length; j++) {
-			students[0][j] = students[0][j - 1] + (student.charAt(j));
+		String[] students = new String[studentNum]; // 00 때문에 int X
+		for (int i = 0; i < studentNum; i++) {
+			students[i] = br.readLine();
+		}
+		// set 자료 구조 이용
+		// Set<String> set = new HashSet<>();
+		int len = students[0].length();
+		for (int i = len - 1; i >= 0; i--) {
+			Set<String> set = new HashSet<>();
+			for (int j = 0; j < studentNum; j++) {
+				set.add(students[j].substring(i));
+			}
+			if (set.size() == studentNum) {
+				System.out.println(len - i);
+				return;
+			}
+			// set.clear();
 		}
 
-		for (int i = 1; i < studentNum; i++) {
-			student = br.readLine();
-			students[i][0] = (student.charAt(0) + "");
-			for (int j = 1; j < length; j++) {
-				students[i][j] = students[i][j - 1] + (student.charAt(j));
-			}
-		}
-
-		for (int i = 0; i < length; i++) {
-			boolean isSame = true;
-			for (int j = 1; j < studentNum; j++) {
-				if (!students[j][length - 1 - i].equals(students[j - 1][length - 1 - i])) {
-					isSame = false;
-					break;
-				}
-			}
-			if (isSame) {
-				System.out.println(i + 1);
-				break;
-			}
-		}
+		// 3중 for 문 사용해 전체 비교하는 방식
+		// String student = br.readLine();
+		// int length = student.length();
+		// String[][] students = new String[studentNum][length];
+		// students[0][0] = student.charAt(length - 1) + "";
+		// for (int j = 1; j < length; j++) {
+		// 	students[0][j] = student.charAt(length - 1 - j) + students[0][j - 1];
+		// }
+		//
+		// for (int i = 1; i < studentNum; i++) {
+		// 	student = br.readLine();
+		// 	students[i][0] = student.charAt(length - 1) + "";
+		// 	for (int j = 1; j < length; j++) {
+		// 		students[i][j] = student.charAt(length - 1 - j) + students[i][j - 1];
+		// 	}
+		// }
+		//
+		// for (int numIdx = 0; numIdx < length; numIdx++) {
+		// 	boolean isSame = false;
+		// 	for (int stdStd = 0; stdStd < studentNum; stdStd++) {
+		// 		String cur = students[stdStd][numIdx];
+		// 		for (int tempStd = stdStd + 1; tempStd < studentNum; tempStd++) {
+		// 			String temp = students[tempStd][numIdx];
+		// 			if (cur.equals(temp)) {
+		// 				isSame = true;
+		// 				break;
+		// 			}
+		// 		}
+		// 	}
+		// 	if (!isSame) {
+		// 		System.out.println(numIdx + 1);
+		// 		break;
+		// 	}
+		// }
 
 	}
 }
