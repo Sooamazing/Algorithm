1cf0de58806ecb67acd83b3594d90f2d5ab3c6b7
Sooamazing <honang515@gmail.com>
2024-11-04 23:44:01 +0900

    백준 - 단어 길이 재기 - 단순 string.length()

---
+++ Baekjoon/math/MeasureWordLength.java
@@ -0,0 +1,15 @@
+package Baekjoon.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 24-11-04 1min
+// https://www.acmicpc.net/problem/2743
+public class MeasureWordLength {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println(br.readLine().length());
+	}
+}
