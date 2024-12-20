bc907315bbeb95eccf0c8b58bacc704d2ee81e54
Sooamazing <honang515@gmail.com>
2024-12-20 23:43:46 +0900

    백준 - 마이크로소프트 로고 - 단순 출력

---
+++ Baekjoon/simulation/MicrosoftLogo.java
@@ -0,0 +1,15 @@
+package Baekjoon.simulation;
+
+// 24-12-20 3min
+public class MicrosoftLogo {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("       _.-;;-._\n");
+		sb.append("'-..-'|   ||   |\n");
+		sb.append("'-..-'|_.-;;-._|\n");
+		sb.append("'-..-'|   ||   |\n");
+		sb.append("'-..-'|_.-''-._|");
+		System.out.println(sb);
+
+	}
+}
