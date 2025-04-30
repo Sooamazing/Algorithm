ab11d6e09c2ed1e450d69a382a02c4c9569467f8
Sooamazing <honang515@gmail.com>
2025-04-30 23:41:38 +0900

    백준 - 콜센터 - 단순 출력

---
+++ Baekjoon/simulation/CallCenter.java
@@ -0,0 +1,21 @@
+package Baekjoon.simulation;
+
+// 25-04-30 3min
+// https://www.acmicpc.net/problem/5339
+public class CallCenter {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("     /~\\\n")
+			.append("    ( oo|\n")
+			.append("    _\\=/_\n")
+			.append("   /  _  \\\n")
+			.append("  //|/.\\|\\\\\n")
+			.append(" ||  \\ /  ||\n")
+			.append("============\n")
+			.append("|          |\n")
+			.append("|          |\n")
+			.append("|          |\n");
+
+		System.out.println(sb);
+	}
+}
