2b52e4a8a26b6dbbe18d9efdee302893c251647c
Sooamazing <honang515@gmail.com>
2025-05-17 21:27:45 +0900

    백준 - 홍익대학교 - 단순 년도 차 계산

---
+++ Baekjoon/math/HongikUniv.java
@@ -0,0 +1,16 @@
+package Baekjoon.math;
+
+import java.util.Scanner;
+
+// 25-05-17 5min
+// https://www.acmicpc.net/problem/16394
+public class HongikUniv {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int current = Integer.parseInt(scanner.nextLine());
+		int result = current - 1946;
+
+		System.out.println(result);
+	}
+}
