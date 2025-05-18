5d8956fa116794f9e8834e20e7d76900dc628587
Sooamazing <honang515@gmail.com>
2025-05-18 23:43:20 +0900

    백준 - 제리와 톰 - 단순 차이 계산

---
+++ Baekjoon/math/JerryAndTom.java
@@ -0,0 +1,20 @@
+package Baekjoon.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 25-05-18 5min
+// https://www.acmicpc.net/problem/16430
+public class JerryAndTom {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+
+		System.out.println((b - a) + " " + b);
+	}
+}
