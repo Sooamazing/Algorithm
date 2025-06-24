c7350c5c2a209f55a33b68c0dc2d7cfc3833586c
Sooamazing <honang515@gmail.com>
2025-06-24 23:54:18 +0900

    백준 - 새로운 시작 - 단순 문자열 출력

---
+++ Baekjoon/Implement/NewStart.java
@@ -0,0 +1,10 @@
+package Baekjoon.Implement;
+
+// 25-06-24 1min
+// https://www.acmicpc.net/problem/15962
+public class NewStart {
+
+    public static void main(String[] args) {
+        System.out.println("파이팅!!");
+    }
+}
