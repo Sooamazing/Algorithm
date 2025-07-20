a9f92c4f47cf4cc7ccd71d4a81e902fea4c7e042
Sooamazing <honang515@gmail.com>
2025-07-20 23:46:46 +0900

    백준 - SciComLove - 반복 출

---
+++ Baekjoon/Implement/SciComLove.java
@@ -0,0 +1,19 @@
+package Baekjoon.Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 25-07-20 1min
+// https://www.acmicpc.net/problem/21598
+public class SciComLove {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int total = Integer.parseInt(br.readLine());
+
+        System.out.println("SciComLove\n".repeat(total));
+
+    }
+}
