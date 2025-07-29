b13074800338da7795a872d35910a7707b145f73
Sooamazing <honang515@gmail.com>
2025-07-30 00:09:45 +0900

    백준 - Sounds fishy! #1 - 전체 경우의 수 구 - #1 등

--- Baekjoon/Implement/SoundsFishy.java
+++ Baekjoon/Implement/SoundsFishy.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 25-07-29 27
+// 25-07-29 20min
 // https://www.acmicpc.net/problem/6764
 public class SoundsFishy {
 
@@ -16,30 +16,37 @@ public class SoundsFishy {
         boolean isUp = false;
         boolean isDown = false;
         boolean isConstant = false;
+        boolean hasNoFish = false;
         for (int i = 1; i < 4; i++) {
             int current = Integer.parseInt(br.readLine());
             if (i < 2) {
                 isUp = prev < current;
                 isDown = prev > current;
                 isConstant = prev == current;
+                prev = current;
                 continue;
             }
             if (isUp) {
                 if ((prev >= current)) {
-                    System.out.println("No Fish.");
+                    hasNoFish = true;
+                    break;
                 }
             } else if (isDown) {
                 if ((prev <= current)) {
-                    System.out.println("No Fish.");
+                    hasNoFish = true;
+                    break;
                 }
-            } else if (isConstant) {
+            } else {
                 if (prev != current) {
-                    System.out.println("No Fish");
+                    hasNoFish = true;
+                    break;
                 }
             }
+            prev = current;
         }
-
-        if (isUp) {
+        if (hasNoFish) {
+            System.out.println("No Fish");
+        } else if (isUp) {
             System.out.println("Fish Rising");
         } else if (isDown) {
             System.out.println("Fish Diving");
