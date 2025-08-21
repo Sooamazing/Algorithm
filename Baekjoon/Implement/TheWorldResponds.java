79799bf63d6b783b4bdd49678086fbaafe883466
Sooamazing <honang515@gmail.com>
2025-08-21 23:41:33 +0900

    백준 - The World Responds - 단순 출

---
+++ Baekjoon/Implement/TheWorldResponds.java
@@ -0,0 +1,11 @@
+package Baekjoon.Implement;
+
+// 25-08-21 1min
+// https://www.acmicpc.net/problem/23234
+public class TheWorldResponds {
+
+    public static void main(String[] args) {
+
+        System.out.println("The world says hello!");
+    }
+}
