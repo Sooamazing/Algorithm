796cb4461d718391795ba9be1463a59577d7f804
Sooamazing <honang515@gmail.com>
2025-09-20 20:32:20 +0900

    백준 - 나는 누구인가 - 단순 출

---
+++ Baekjoon/Implement/WhoAmI.java
@@ -0,0 +1,11 @@
+package Baekjoon.Implement;
+
+// 25-09-20 1min
+// https://www.acmicpc.net/problem/15733
+public class WhoAmI {
+
+    public static void main(String[] args) {
+
+        System.out.println("I'm Sexy");
+    }
+}
