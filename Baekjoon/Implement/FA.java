53db8a288b8672317faaf90ca689be84a2bc31b2
Sooamazing <honang515@gmail.com>
2025-09-28 16:10:33 +0900

    백준 - FA - 단순 논리, 추

---
+++ Baekjoon/Implement/FA.java
@@ -0,0 +1,12 @@
+package Baekjoon.Implement;
+
+// 25-09-28 5min
+// https://www.acmicpc.net/problem/14935
+// 참고: https://www.acmicpc.net/board/view/55391
+public class FA {
+
+    public static void main(String[] args) {
+
+        System.out.println("FA");
+    }
+}
