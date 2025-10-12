5c74d38fb6ee011a6fcc89a25c909561fee6dc8d
Sooamazing <honang515@gmail.com>
2025-10-12 19:15:52 +0900

    백준 - 와이버스 부릉부릉 - ONLY 출력

---
+++ Baekjoon/Implement/WiBusVroomVroom.java
@@ -0,0 +1,11 @@
+package Baekjoon.Implement;
+
+// 25-10-12 1min
+// https://www.acmicpc.net/problem/14645
+public class WiBusVroomVroom {
+
+    public static void main(String[] args) {
+
+        System.out.println("비와이");
+    }
+}
