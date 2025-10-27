44e06287c9e67a21411572ac1a280d61204495f4
Sooamazing <honang515@gmail.com>
2025-10-27 20:20:49 +0900

    백준 - CAPS - 단순 upper case

---
+++ Baekjoon/Implement/CAPS.java
@@ -0,0 +1,17 @@
+package Baekjoon.Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 25-10-27 1min
+// https://www.acmicpc.net/problem/15000
+public class CAPS {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().toUpperCase());
+
+    }
+}
