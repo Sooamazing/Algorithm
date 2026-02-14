e0da6fef751182a3dfac19ac38ebb781c788c35c
Sooamazing <honang515@gmail.com>
2026-02-14 22:25:58 +0900

    백준 - Rook #1 - 단순 출력 - 등수 #1 등

---
+++ Baekjoon/Implement/Rook.java
@@ -0,0 +1,26 @@
+package Baekjoon.Implement;
+
+// 26-02-14 1min
+// https://www.acmicpc.net/problem/2393
+public class Rook {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("  ___  ___  ___\n"
+                + "  | |__| |__| |\n"
+                + "  |           |\n"
+                + "   \\_________/\n"
+                + "    \\_______/\n"
+                + "     |     |\n"
+                + "     |     |\n"
+                + "     |     |\n"
+                + "     |     |\n"
+                + "     |_____|\n"
+                + "  __/       \\__\n"
+                + " /             \\\n"
+                + "/_______________\\");
+
+        System.out.println(sb);
+    }
+}
