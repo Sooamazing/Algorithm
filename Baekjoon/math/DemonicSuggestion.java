b5a73e06b8fd38db96fc8332e4c0e9f75832b2c2
Sooamazing <honang515@gmail.com>
2026-02-26 00:25:14 +0900

    백준 - 악마의 제안 - 오버플로우 유의 - 수학 식 계산

--- Baekjoon/math/DemonicSuggestion.java
+++ Baekjoon/math/DemonicSuggestion.java
@@ -5,39 +5,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 26-02-25 40
+// 26-02-25 45min
 // https://www.acmicpc.net/problem/23972
-// 참고: https://propercoding.tistory.com/320#google_vignette
+// 반례: https://www.acmicpc.net/board/view/102974
 public class DemonicSuggestion {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int pay = Integer.parseInt(st.nextToken());
-        int multiple = Integer.parseInt(st.nextToken());
+        long pay = Integer.parseInt(st.nextToken());
+        long multiple = Integer.parseInt(st.nextToken());
 
-        if (pay % multiple == 0) {
-            System.out.println(pay);
-        } else {
-            // 20 5
-            // 20 / (5 + 1) = 3
-            // 3 * (5 + 1) = 18
-            // 18 + 3 = 21
-            // 21 + 5 = 26
-
-            // 25 -> 25
-            // 24 -> 20 => bingo
-
-            // 21 5
-            // 21 / 5 = 4
-            // 4 * 5 = 20
-            // 20 + 4 = 24
-            // 24 + 5 = 29
-
-            // 25 -> 20
-            // 26 -> 25 => bingo
-            System.out.println((pay / (multiple + 1)) + multiple);
+        if (multiple == 1) {
+            System.out.println(-1);
+            return;
         }
+        System.out.println(
+                pay * multiple / (multiple - 1)
+                        + ((pay * multiple % (multiple - 1) == 0) ? 0 : 1)
+        );
+
     }
 }
