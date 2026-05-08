d0f201c4de7de91f79ab60d3d5dbc9ce8b2a9bb9
Sooamazing <honang515@gmail.com>
2026-05-09 00:03:08 +0900

    Leetcode - 608. Tree Node - NOT IN 으로 바꾸고 싶다면 null 제외 필요

--- LeetCode/SQL/608_TreeNode.sql
+++ LeetCode/SQL/608_TreeNode.sql
@@ -12,3 +12,15 @@ select id,
            ELSE 'Leaf'
            END AS 'type'
 FROM Tree
+
+-- not in 사용 시 NULL이 존재하면 전체 결과가 UNKNOWN이 되어 아무것도 반환하지 않음 -> COALESCE로 NULL을 ''로 바꿔주면 해결. 권장 X
+select id,
+       CASE
+           WHEN p_id IS NULL THEN 'Root'
+           WHEN id not in (SELECT COALESCE(p_id, '') from tree) THEN 'Leaf'
+           ELSE 'Inner'
+           END AS 'type'
+FROM Tree
+
+-- 아래도 가능
+-- WHEN id NOT IN (SELECT DISTINCT p_id FROM Tree WHERE p_id IS NOT NULL) THEN 'Leaf'
