ebd61eb631a2f8b8afd40c3f5fbb54df358ec25d
Sooamazing <honang515@gmail.com>
2026-06-07 00:02:38 +0900

    Leetcode - 2574. Left and Right Sum Differences - 누적합. - 정정. leftSum, rightSum 배열 아니어도 구할 수 있음.

--- LeetCode/java/LeftandRightSumDifferences_2574.java
+++ LeetCode/java/LeftandRightSumDifferences_2574.java
@@ -1,26 +1,27 @@
 package LeetCode.java;
 
-// 26-06-06
+// 26-06-06 10min
 // https://leetcode.com/problems/left-and-right-sum-differences
 class LeftandRightSumDifferences_2574 {
 
     public int[] leftRightDifference(int[] nums) {
 
-        int n = nums.length;
-        int[] ans = new int[n];
-
-        int leftSum = 0;
-        for (int i = 0; i < n; ++i) {
-            ans[i] = leftSum;
-            leftSum += nums[i];
+        int numLength = nums.length;
+        int[] leftSum = new int[numLength];
+        int[] rightSum = new int[numLength];
+        int[] answer = new int[numLength];
+        for (int i = 1; i < numLength; i++) {
+            leftSum[i] = leftSum[i - 1] + nums[i - 1];
+        }
+        for (int i = numLength - 2; i >= 0; i--) {
+            rightSum[i] = rightSum[i + 1] + nums[i + 1];
         }
 
-        int rightSum = 0;
-        for (int i = n - 1; i >= 0; --i) {
-            ans[i] = Math.abs(ans[i] - rightSum);
-            rightSum += nums[i];
+        for (int i = 0; i < numLength; i++) {
+            answer[i] = Math.abs(leftSum[i] - rightSum[i]);
         }
 
-        return ans;
+        return answer;
+
     }
 }
