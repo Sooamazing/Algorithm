652eb3239c3a341be37630b81f8140caeadff94e
Sooamazing <honang515@gmail.com>
2026-06-23 22:22:20 +0900

    Leetcode - 190. Reverse Bits - binary reverse

---
+++ LeetCode/java/ReverseBits_190.java
@@ -0,0 +1,11 @@
+package LeetCode.java;
+
+// 26-06-23 3min
+// https://leetcode.com/problems/reverse-bits
+public class ReverseBits_190 {
+
+    public int reverseBits(int n) {
+
+        return Integer.reverse(n);
+    }
+}
