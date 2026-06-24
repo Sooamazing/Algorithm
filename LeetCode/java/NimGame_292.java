31922c33debefbfefc2f209e6327877aa9ec15ea
Sooamazing <honang515@gmail.com>
2026-06-24 23:45:03 +0900

    Leetcode - 292. Nim Game - 모래성 가져가기

---
+++ LeetCode/java/NimGame_292.java
@@ -0,0 +1,11 @@
+package LeetCode.java;
+
+// 26-06-24 8min
+// https://leetcode.com/problems/nim-game
+public class NimGame_292 {
+
+    public boolean canWinNim(int n) {
+
+        return n % 4 != 0;
+    }
+}
