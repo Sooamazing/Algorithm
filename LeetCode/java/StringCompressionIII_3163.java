94ec8c4fb806c775f9d1dc071b4d9c450a2dd1fc
Sooamazing <honang515@gmail.com>
2026-07-01 00:08:29 +0900

    Leetcode - 3163. String Compression III - 조건 구현

--- LeetCode/java/StringCompressionIII_3163.java
+++ LeetCode/java/StringCompressionIII_3163.java
@@ -1,68 +1,40 @@
 package LeetCode.java;
 
-// 26-06-30
+// 26-06-30 20min
 // https://leetcode.com/problems/string-compression-iii/
 public class StringCompressionIII_3163 {
 
-// class Solution {
-//     public String compressedString(String word) {
-//         StringBuilder sb = new StringBuilder();
-
-//         int count = 0;
-//         char prev = word.charAt(0);
-//         for(int i = 1; i<word.length();i++){
-//             char c = word.charAt(i);
-//             count++;
-//             if(prev == c){
-//                 if(count == 9){
-//                     sb.append(9).append(c);
-//                     prev = c;
-//                     count = 1;
-//                 }
-//             }else{
-//                 sb.append(count).append(prev);
-//                 prev = c;
-//                 count = 1;
-//             }
-//         }
-//             char c = word.charAt(word.length()-1);
-//                 if(count == 9){
-//                     sb.append(9).append(c);
-//                 }else{
-//                     sb.append(count).append(prev);
-//                 }
-
-//         return sb.toString().trim();
-//     }
-// }
-
     public String compressedString(String word) {
 
-        StringBuilder comp = new StringBuilder("");
+        StringBuilder sb = new StringBuilder();
 
-        // pos tracks our position in the input string
-        int pos = 0;
-
-        // Process until we reach end of string
-        while (pos < word.length()) {
-            int consecutiveCount = 0;
-
-            char currentChar = word.charAt(pos);
-
-            // Count consecutive occurrences (maximum 9)
-            while (
-                    pos < word.length() &&
-                            consecutiveCount < 9 &&
-                            word.charAt(pos) == currentChar
-            ) {
-                consecutiveCount++;
-                pos++;
+        int count = 1;
+        char prev = word.charAt(0);
+        for (int i = 1; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (prev == c) {
+                count++;
+            } else {
+                while (count > 9) {
+                    sb.append(9).append(prev);
+                    count -= 9;
+                }
+                if (count > 0) {
+                    sb.append(count).append(prev);
+                }
+                prev = c;
+                count = 1;
             }
-
-            // Append count followed by character to result
-            comp.append(consecutiveCount).append(currentChar);
+        }
+        while (count > 9) {
+            sb.append(9).append(prev);
+            count -= 9;
+        }
+        if ((word.length() > 1 && word.charAt(word.length() - 1) == word.charAt(word.length() - 2))
+                || count == 1) {
+            sb.append(count).append(prev);
         }
 
-        return comp.toString();
+        return sb.toString().trim();
     }
 }
