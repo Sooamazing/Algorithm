cf43c298e0f9daf977527d5000b44d2b76d796c6
Sooamazing <honang515@gmail.com>
2026-08-02 23:59:59 +0900

    Leetcode - 877. Stone Game - math, dp - answer

---
+++ LeetCode/java/StoneGame_877.java
@@ -0,0 +1,11 @@
+package LeetCode.java;
+
+// 26-08-02 5min
+// https://leetcode.com/problems/stone-game
+public class StoneGame_877 {
+
+    public boolean stoneGame(int[] piles) {
+
+        return true;
+    }
+}
