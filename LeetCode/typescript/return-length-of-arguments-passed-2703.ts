90de801ad690a495dec1aad4aa6c633afd0fcfa2
Sooamazing <honang515@gmail.com>
2026-08-17 13:44:54 +0900

    Leetcode - 2703. Return Length of Arguments Passed - array length

---
+++ LeetCode/typescript/return-length-of-arguments-passed-2703.ts
@@ -0,0 +1,11 @@
+// 26-08-17 2min
+// https://leetcode.com/problems/return-length-of-arguments-passed
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+  return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
