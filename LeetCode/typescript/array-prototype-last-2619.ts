b3cf41c0706a8f5c49223ca6a8a6c22059c898c9
Sooamazing <honang515@gmail.com>
2026-08-18 21:55:06 +0900

    Leetcode - 2619. Array Prototype Last - array, prototype, this

---
+++ LeetCode/typescript/array-prototype-last-2619.ts
@@ -0,0 +1,18 @@
+// 26-08-18 3min
+// https://leetcode.com/problems/array-prototype-last
+
+interface Array<T> {
+  last(): T | -1;
+}
+
+Array.prototype.last = function () {
+  if (this.length === 0) {
+    return -1;
+  }
+  return this[this.length - 1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
