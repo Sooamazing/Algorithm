587c4e41d2652d3ab3291763a1d069e13b2f1a1f
Sooamazing <honang515@gmail.com>
2026-08-21 22:27:17 +0900

    Leetcode - 2626. Array Reduce Transformation - js, reduce

---
+++ LeetCode/typescript/array-reduce-transformation-2626.ts
@@ -0,0 +1,12 @@
+// 26-08-21 5min
+// https://leetcode.com/problems/array-reduce-transformation/
+
+type Fn = (accum: number, curr: number) => number
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+  let sum = init;
+  for (let i = 0; i < nums.length; i++) {
+    sum = fn(sum, nums[i]);
+  }
+  return sum;
+};
