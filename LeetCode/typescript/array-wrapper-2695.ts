6ce8875499d6e4da52495debb5b431d1b638a04c
Sooamazing <honang515@gmail.com>
2026-08-28 22:09:12 +0900

    Leetcode - 2695. Array Wrapper - js, array, reduce, join

---
+++ LeetCode/typescript/array-wrapper-2695.ts
@@ -0,0 +1,27 @@
+// 26-08-28 10min
+// https://leetcode.com/problems/array-wrapper
+
+class ArrayWrapper {
+
+  nums: number[];
+
+  constructor(nums: number[]) {
+    this.nums = nums;
+  }
+
+  valueOf(): number {
+    return this.nums.reduce((sum, num) => sum + num, 0);
+  }
+
+  toString(): string {
+    return "[" + this.nums.toString() + "]";
+  }
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
